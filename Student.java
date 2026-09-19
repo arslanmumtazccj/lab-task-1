50998d834d356445c2abd14798edab91c706293d
Arslan Mumtaz <arslanmumtazccj@gmail.com>
2026-09-19 18:35:16 +0500

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,10 @@
+public class Student{
+
+	String studentId;
+ 	String name;
+ 	int completedCredits;
+	
+
+
+
+}
---
+++ task1demo.java
@@ -0,0 +1,54 @@
+public class task1demo{
+
+	public static void main(String args[]){
+	Student s1 = new Student();
+        Student s2 = new Student();
+        Student s3 = new Student();
+
+ //Student 1:
+
+	s1.name = "Abdul Rehman Azam";
+	s1.studentId = "SP26-BAI-003";
+	s1.completedCredits = 16;
+
+//student 2:
+
+	s2.name = "Ahmad Talal";
+	s2.studentId = "SP26-BAI-008";
+	s2.completedCredits = 17;
+
+//student 3:
+	
+	s3.name = "Adnan";
+	s3.studentId = "SP26-BAI-033";
+	s3.completedCredits = 18;
+
+
+
+	System.out.println(s1.name);
+	System.out.println(s1.studentId);
+	System.out.println(s1.completedCredits);
+
+
+	System.out.println(s2.name);
+	System.out.println(s2.studentId);
+	System.out.println(s2.completedCredits);
+
+	System.out.println(s3.name);
+	System.out.println(s3.studentId);
+	System.out.println(s3.completedCredits);
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+}
